dec1e5c36aa8ec49e9e5d0a8752cefaaf41664eb
igorparente <igorscheidegger@hotmail.com>
2021-08-03 15:59:13 -0300

    cloudinary done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,30 @@ class CommentsController < ApplicationController
   #   @post = Post.find(params[:post_id])
   # end
 
+  def upvote
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.post = @post
+    if current_user.voted_up_on? @comment
+      @comment.unvote_by current_user
+    else
+      @comment.upvote_by current_user
+    end
+    render "vote.js.erb"
+  end
+
+  def downvote
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.post = @post
+    if current_user.voted_down_on? @comment
+      @comment.unvote_by current_user
+    else
+      @comment.downvote_by current_user
+    end
+    render "vote.js.erb"
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  has_many_attached :photos
-
   acts_as_votable
+
+  has_many_attached :photos
 end
